5067b90445846a65f1221782b2f6280578958d5c
gonzalezFernandez <andres.gonzalez-fernandez@capgemini.com>
2022-03-18 10:12:15 +0100

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesModule } from './components/clientes/clientes.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
